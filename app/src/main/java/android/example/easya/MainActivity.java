976483c443e077a0e62fcc094b385bffcc808a09
ShylaMo <chaima.moutachaouiq@gmail.com>
2021-02-03 15:45:48 +0100

    EasyA

---
+++ app/src/main/java/android/example/easya/MainActivity.java
@@ -0,0 +1,15 @@
+package android.example.easya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
