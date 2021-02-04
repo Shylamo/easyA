4dc67419d8b3ff90bd4c39a756fe0b3429066651
ShylaMo <chaima.moutachaouiq@gmail.com>
2021-02-04 23:27:46 +0100

    EasyA

---
+++ app/src/main/java/android/example/easya/succes_reinitialisation.java
@@ -0,0 +1,14 @@
+package android.example.easya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class succes_reinitialisation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succes_reinitialisation);
+    }
+}
